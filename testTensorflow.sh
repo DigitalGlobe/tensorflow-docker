446f772288cdabf0cb43c1e4f9532007dd434f6f
michael.trotter <michael.trotter@digitalglobe.com>
2016-08-16 11:31:54 -0400

    Adding status.json write

--- testTensorflow.sh
+++ testTensorflow.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 OUTPUT=/mnt/work/output/dump
+STATUS_JSON=/mnt/work/status.json
 
 mkdir -p "${OUTPUT}"
 
 python -c "import tensorflow" > "${OUTPUT}/tensorflow" 2>&1
+
+# write status.json
+if [ $? -eq 0 ]
+then
+  printf "{\"status\": \"success\", \"reason\": \"Successfully imported tensorflow\"}" > "${STATUS_JSON}"
+else
+  printf "{\"status\": \"failed\", \"reason\": \"Failed to import tensorflow\"}" > "${STATUS_JSON}"
+fi
